7d64a609516e91ad2e8c690e57000e4461969cbf
Aleksander Linczuk <aleksanderlinczuk@gmail.com>
2024-03-23 18:46:02 +0100

    added item view

--- tmawarehouse/src/main/java/com/smartbusiness/tmawarehouse/views/list/ListView.java
+++ tmawarehouse/src/main/java/com/smartbusiness/tmawarehouse/views/list/ListView.java
@@ -2,31 +2,46 @@ package com.smartbusiness.tmawarehouse.views.list;
 
 
 import com.smartbusiness.tmawarehouse.model.entity.ItemEntity;
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
 @PageTitle("TMA warehouse")
 @Route(value = "")
 public class ListView extends VerticalLayout {
     Grid<ItemEntity> grid = new Grid<>(ItemEntity.class);
     TextField filterText = new TextField();
+    ItemForm itemForm;
+
 
     public ListView() {
         addClassName("list-view");
         setSizeFull();
         configureGrid();
+        configureItemForm();
 
-        add(getToolbar(), grid);
+
+        add(getToolbar(), getContent());
+    }
+
+    private Component getContent() {
+        HorizontalLayout content = new HorizontalLayout(grid, itemForm);
+        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, itemForm);
+        content.addClassNames("content");
+        content.setSizeFull();
+        return content;
+    }
+
+    private void configureItemForm() {
+        itemForm = new ItemForm();
+        itemForm.setWidth("25em");
     }
 
     private void configureGrid() {
